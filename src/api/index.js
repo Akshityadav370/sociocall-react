5344aff7c63f2ec293ed325e0ba92b1a9f2c1b62
Akshit Yadav <akshit07032001@gmail.com>
2023-08-09 06:57:14 +0530

    used customFetch to retrieving posts

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
-import { LOCALSTORAGE_TOKEN_KEY } from '../utils';
+import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from '../utils';
 
 const customFetch = async (url, { body, ...customConfig }) => {
   const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
 
+  // Default headers
   const headers = {
     'content-type': 'application/json',
     Accept: 'application/json',
@@ -12,6 +13,7 @@ const customFetch = async (url, { body, ...customConfig }) => {
     headers.Authorization = `Bearer ${token}`;
   }
 
+  // CustomConfig contains method:POST, header:{}
   const config = {
     ...customConfig,
     headers: {
@@ -20,6 +22,7 @@ const customFetch = async (url, { body, ...customConfig }) => {
     },
   };
 
+  // body is an object like {email: , password:  ,}
   if (body) {
     config.body = JSON.stringify(body);
     // stringify converts JSON object to string we need to do this to make the API URL call
@@ -27,10 +30,10 @@ const customFetch = async (url, { body, ...customConfig }) => {
 
   try {
     const response = await fetch(url, config);
-    const data = response.json();
+    const data = await response.json();
     // .json() function reads the response body and parses it as JSON
 
-    if (response.success) {
+    if (data.success) {
       return {
         data: data.data,
         success: true,
@@ -48,6 +51,8 @@ const customFetch = async (url, { body, ...customConfig }) => {
   }
 };
 
-const getPosts = (page, limit) => {
-  return customFetch();
+export const getPosts = (page = 1, limit = 5) => {
+  return customFetch(API_URLS.posts(page, limit), {
+    method: 'GET',
+  });
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
-
+import { getPosts } from '../api';
+import { useState, useEffect } from 'react';
 
 function App() {
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+      console.log('response', response);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
