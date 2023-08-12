612f45f845e8ef9a943aeee220b08c6ec2473b5a
Akshit Yadav <akshit07032001@gmail.com>
2023-08-12 14:52:25 +0530

    created global post state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,24 @@
 import styles from '../styles/home.module.css';
-import { Comment, Loader, CreatePost } from '../components';
+import { Comment, Loader, CreatePost, FriendsList } from '../components';
 import { useState, useEffect } from 'react';
 import { getPosts } from '../api';
 import { Link } from 'react-router-dom';
-import { useAuth } from '../hooks';
-import { FriendsList } from '../components';
+import { useAuth, usePosts } from '../hooks';
+
 
 const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  
   const auth = useAuth();
+  const posts = usePosts();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await getPosts();
-      // console.log('response', response);
-
-      if (response.success) {
-        setPosts(response.data.posts);
-      }
-
-      setLoading(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  if (loading) {
+  if (posts.loading) {
     return <Loader />;
   }
   return (
     <div className={styles.home}>
       <div className={styles.postsList}>
         <CreatePost/>
-        {posts.map((post) => (
+        {posts.data.map((post) => (
           <div className={styles.postWrapper} key={`post-${post._id}`}>
             <div className={styles.postHeader}>
               <div className={styles.postAvatar}>
