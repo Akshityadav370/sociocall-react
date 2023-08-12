7c76ecc30dad06b676fb76baa71c901e366b5250
Akshit Yadav <akshit07032001@gmail.com>
2023-08-12 13:57:12 +0530

    created friendslist on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,13 @@ import { Comment, Loader } from '../components';
 import { useState, useEffect } from 'react';
 import { getPosts } from '../api';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks';
+import { FriendsList } from '../components';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const auth = useAuth();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -27,61 +30,64 @@ const Home = () => {
     return <Loader />;
   }
   return (
-    <div className={styles.postsList}>
-      {posts.map((post) => (
-        <div className={styles.postWrapper} key={`post-${post._id}`}>
-          <div className={styles.postHeader}>
-            <div className={styles.postAvatar}>
-              <img
-                src="https://www.svgrepo.com/show/384669/account-avatar-profile-user-13.svg"
-                alt="user-pic"
-              />
-              <div>
-                <Link
-                  state={{
-                    user: post.user,
-                  }}
-                  to={{
-                    pathname: `/user/${post.user._id}`,
-                  }}
-                  className={styles.postAuthor}
-                >
-                  {post.user.name}
-                </Link>
-                <span className={styles.postTime}>a minute ago</span>
-              </div>
-            </div>
-            <div className={styles.postContent}>{post.content}</div>
-
-            <div className={styles.postActions}>
-              <div className={styles.postLike}>
+    <div className={styles.home}>
+      <div className={styles.postsList}>
+        {posts.map((post) => (
+          <div className={styles.postWrapper} key={`post-${post._id}`}>
+            <div className={styles.postHeader}>
+              <div className={styles.postAvatar}>
                 <img
-                  src="https://www.svgrepo.com/show/1198/like.svg"
-                  alt="likes-icon"
+                  src="https://www.svgrepo.com/show/384669/account-avatar-profile-user-13.svg"
+                  alt="user-pic"
                 />
-                <span>5</span>
+                <div>
+                  <Link
+                    state={{
+                      user: post.user,
+                    }}
+                    to={{
+                      pathname: `/user/${post.user._id}`,
+                    }}
+                    className={styles.postAuthor}
+                  >
+                    {post.user.name}
+                  </Link>
+                  <span className={styles.postTime}>a minute ago</span>
+                </div>
+              </div>
+              <div className={styles.postContent}>{post.content}</div>
+
+              <div className={styles.postActions}>
+                <div className={styles.postLike}>
+                  <img
+                    src="https://www.svgrepo.com/show/1198/like.svg"
+                    alt="likes-icon"
+                  />
+                  <span>5</span>
+                </div>
+
+                <div className={styles.postCommentsIcon}>
+                  <img
+                    src="https://www.svgrepo.com/show/493993/comments.svg"
+                    alt="comments-icon"
+                  />
+                  <span>2</span>
+                </div>
+              </div>
+              <div className={styles.postCommentBox}>
+                <input placeholder="Start typing a comment" />
               </div>
 
-              <div className={styles.postCommentsIcon}>
-                <img
-                  src="https://www.svgrepo.com/show/493993/comments.svg"
-                  alt="comments-icon"
-                />
-                <span>2</span>
+              <div className={styles.postCommentsList}>
+                {post.comments.map((comment) => (
+                  <Comment comment={comment} key={`comment-${comment._id}`} />
+                ))}
               </div>
-            </div>
-            <div className={styles.postCommentBox}>
-              <input placeholder="Start typing a comment" />
-            </div>
-
-            <div className={styles.postCommentsList}>
-              {post.comments.map((comment) => (
-                <Comment comment={comment} key={`comment-${comment._id}`} />
-              ))}
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      {auth.user && <FriendsList />}
     </div>
   );
 };
