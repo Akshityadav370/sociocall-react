767d20a02ee905d855ccec0cc51cc6fae8526b0f
Akshit Yadav <akshit07032001@gmail.com>
2023-08-12 14:57:49 +0530

    updating post global state

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -2,10 +2,13 @@ import { useState } from 'react';
 import styles from '../styles/home.module.css';
 import { addPost } from '../api';
 import { toast } from 'react-toastify';
+import { usePosts } from '../hooks';
 
 const CreatePost = () => {
   const [post, setPost] = useState('');
   const [addingPost, setAddingPost] = useState(false);
+
+  const posts = usePosts();
 
   const handleAddPostClick = async () => {
     setAddingPost(true);
@@ -20,6 +23,7 @@ const CreatePost = () => {
 
     if (response.success) {
       setPost('');
+      posts.addPostToState(response.data.post);
       toast.success('Post created successfully!');
     } else {
       toast.error(response.message);
